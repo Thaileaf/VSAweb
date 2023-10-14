3255c64c1eb4c523ccf8706477fd2d576cd5ae78
Thaileaf <lethaison2019@gmail.com>
2023-10-14 19:25:37 -0400

    Maybe Image type will work

--- app/page_render.tsx
+++ app/page_render.tsx
@@ -44,7 +44,7 @@ export default function HomePage() {
           <h1 className={'text-4xl m-8'}>Vietnamese Student Association at NYU</h1>
           <h2 className={'z-10'}>We are the Vietnamese Student Association at New York University. Our group consists of NYU students who are passionate about promoting cultural awareness, increasing ethnic diversity within NYU, and to providing a group platform for all NYU students interested in fostering a better understanding of the Vietnamese experience</h2>
         </div>
-        <img src={'/image.png'} className={'absolute z-0 object-cover'}/>
+        <Image alt={'Pictures of people'} src={'/image.png'} className={'absolute z-0 object-cover'}/>
       </div>
 
 
@@ -53,14 +53,14 @@ export default function HomePage() {
           <h1 className={'text-4xl m-8'}>Our Mission</h1>
           <h2 className={'z-10'}>At the heart of our mission lies the commitment to enriching multi-cultural atmosphere and fostering a deeper appreciation of the vibrant Vietnamese culture at NYU by providing cultural, social, and philanthropic events to the student populations and those who are interested in the Vietnamese heritage.</h2>
         </div>
-        <img src={'/image2.png'} className={'absolute z-0 object-cover'}/>
+        <Image alt={'Pictures of people'} src={'/image2.png'} className={'absolute z-0 object-cover'}/>
       </div>
       <div className={'slide slide3'}>
         <div className={'w-1/2 z-10 text-center text-container'}>
           <h1 className={'text-4xl m-8'}>Get Involved</h1>
           <h2 className={'z-10'}>We are the Vietnamese Student Association at New York University. Our group consists of NYU students who are passionate about promoting cultural awareness, increasing ethnic diversity within NYU, and to providing a group platform for all NYU students interested in fostering a better understanding of the Vietnamese experience</h2>
         </div>
-        <img src={'/image3.png'} width={2000} height={2000} className={'absolute z-0 object-cover'}/>
+        <Image alt={'Pictures of people'} src={'/image3.png'} width={2000} height={2000} className={'absolute z-0 object-cover'}/>
       </div>
 
 
